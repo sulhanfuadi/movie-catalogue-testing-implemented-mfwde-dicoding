868f36812a912f5beca1a8396e9666ede6352be3
sulhanfuadi <138558396+sulhanfuadi@users.noreply.github.com>
2024-04-17 17:43:14 +0700

    Update config.js

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  KEY: 'YOUR_API_KEY',
+  KEY: '0dac03bf764ea5fad39151623dec51f9',
   BASE_URL: 'https://api.themoviedb.org/3/',
   BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
   DEFAULT_LANGUAGE: 'en-us',
